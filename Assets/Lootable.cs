e1fa373f3411bbefd8d0e5cf163a57af2afdb4ba
piem2503 <maxime.piergiovanni@usherbrooke.ca>
2017-07-01 14:53:04 +0200

    Lootable

---
+++ Assets/Lootable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lootable : MonoBehaviour {
+	Collider2D colider;
+	// Use this for initialization
+	void Start () {
+		colider = this.GetComponent<Collider2D> ();
+		colider.isTrigger = true;
+		ContactFilter2D contactFilter = new ContactFilter2D();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		Debug.Log ("[Lootable] OnCollisionEnter2D");
+
+		if (coll.gameObject.tag == "Player") {
+			Debug.Log ("Looted");
+			Destroy (this.gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("[Lootable] OnCollisionEnter2D");
+
+		if (other.gameObject.tag == "Player") {
+			Debug.Log ("Looted");
+			Destroy (this.gameObject);
+		}
+	}
+}
