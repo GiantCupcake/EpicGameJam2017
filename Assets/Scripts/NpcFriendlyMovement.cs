58f07d3282a7bcdad6201e531b2be87088e124c8
MattDirty <mtorsell@student.42.fr>
2017-07-02 14:43:30 +0200

    derp

---
+++ Assets/Scripts/NpcFriendlyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcFriendlyMovement : MonoBehaviour {
+
+	public float roamDistance = 5f;
+	public float speed = 1f;
+
+	private float startingPos = 0f;
+	private float goal;
+
+	// Use this for initialization
+	void Start () {
+		startingPos = transform.position.x;
+
+		goal = startingPos + Random.Range(-(roamDistance), roamDistance);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
